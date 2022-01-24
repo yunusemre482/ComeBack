e0939feebc530d04c06c92040c174afe30e76583
Yunus emre akgün <akgunyunus482@gmail.com>
2022-01-24 21:28:09 +0300

    project setup

---
+++ src/screens/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const HomePage = () => {
+  return (
+    <View>
+      <Text>Yunus Emre Ak gün</Text>
+    </View>
+  );
+};
+
+export default HomePage;
